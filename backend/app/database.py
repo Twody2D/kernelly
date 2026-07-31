5e247bbbc69e847d27810ad1cbecf20ba8158f3c
Twody2D <paha7a146@gmail.com>
2026-07-31 13:34:36 +0300

    fix: move secrets to env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://kernelly:${POSTGRES_PASSWORD}@localhost:5432/kernelly"
+load_dotenv()
+
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
